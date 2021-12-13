419418a111a0e9516c6da8b2cdcfc3ab0aa012c8
Jiwan Heo <jiwanheo123@gmail.com>
2021-12-12 21:27:27 -0500

    bb_quote()
    New function `bb_quote()`

---
+++ R/bb_quote.R
@@ -0,0 +1,43 @@
+#' Function to return a random quote
+#'
+#' This function returns a random quote from
+#' \url{https://breakingbadapi.com}. It first messages the quote and invisibly
+#' returns it as a character.
+#'
+#' @return Invisibly returns the first item holding the quote as a character.
+#' @importFrom magrittr %>%
+#' @export
+#'
+bb_quote <- function() {
+  url <- "https://www.breakingbadapi.com/api/quote/random"
+
+  resp <- httr::GET(url)
+  if (httr::http_type(resp) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+
+  # parse the response, grab the first item in the list of lists
+  parsed <- jsonlite::fromJSON(httr::content(resp, "text"), simplifyVector = FALSE)[[1]]
+
+  # S3 object to return both content and response
+  structure(
+    list(
+      content = parsed,
+      response = resp
+    ),
+    class = "bb_api"
+  )
+
+}
+
+# Print method for bb_api object
+
+#' @noRd
+#' @importFrom utils str
+#' @export
+#'
+print.bb_api <- function(x, ...) {
+  cat("<Breaking Bad ", ">\n", sep = "")
+  str(x$content)
+  invisible(x)
+}
